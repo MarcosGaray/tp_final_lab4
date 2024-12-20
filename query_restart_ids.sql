738779d41e4820396d5e73e6279bd4fda4bc1864
MarcosGaray <edgeprogramacion@gmail.com>
2024-12-19 22:54:09 -0300

    querys y json de insomnia

--- query_restart_ids.sql
+++ query_restart_ids.sql
@@ -1,16 +1,18 @@
 
-delete from reservas
+delete from reservas;
 
-delete from canchas 
+delete from canchas;
 
-select * from canchas
-select * from reservas
+select * from canchas;
+select * from reservas;
+
+delete from reservas where id= 11;
 
 
-ALTER SEQUENCE reservas_id_seq RESTART WITH 1
+ALTER SEQUENCE reservas_id_seq RESTART WITH 1;
 
-ALTER SEQUENCE canchas_id_seq RESTART WITH 1
+ALTER SEQUENCE canchas_id_seq RESTART WITH 1;
 
-drop table reservas
+drop table reservas;
 
-drop table canchas
+drop table canchas;
